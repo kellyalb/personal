85fd834fc077d3c114e32c3baa8257cbb3d84f7f
Kelly Albrecht <kelly@Kellys-iMac.local>
2015-04-20 21:19:56 -0400

    updates
     Committer: Kelly Albrecht <kelly@albre.ch>

--- js/script.js
+++ js/script.js
@@ -1,17 +1,33 @@
 $(document).ready(function(){
-    $window = $(window);
+  $window = $(window);
+  
+  $('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash,
+    $target = $(target);
+
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 900, 'swing', function () {
+      window.location.hash = target;
+    });
+  });
+  
   $('section[data-type="background"]').each(function(){
     var $bgobj = $(this); // assigning the object
                      
-  $(window).scroll(function() {
-    var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+    $(window).scroll(function() {
+      var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
                                                         
-    // Put together our final background position
-    var coords = '50% '+ yPos + 'px';
-                                                                                 
-    // Move the background
-    $bgobj.css({ backgroundPosition: coords });                                                                  }); 
-});    
+      // Put together our final background position
+      var coords = '50% '+ yPos + 'px';
+                                                                                
+      // Move the background
+      $bgobj.css({ backgroundPosition: coords 
+      });                                                                  
+    }); 
+  });    
 });
 
 /* 
